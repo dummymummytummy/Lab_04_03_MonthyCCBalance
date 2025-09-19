d7672654f66cd167adb43ac8d4eb85089d70bbde
Prashna <dhakalph@mail.uc.edu>
2025-09-19 15:05:14 -0400

    Completed Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main{
+    public static void main(String[] args) {
+        int balance = 5000;
+        double rate =  0.17;
+        int interest;
+        System.out.println("Initial balance is : " + balance);
+        interest = (int) (balance * rate);
+        System.out.println("Interest for the first month is : " + interest);
+        balance = balance + interest;
+        System.out.println("Balance after first month is : " + balance);
+        interest = (int)(balance * rate);
+        System.out.println("Interest for the second month is : " + interest);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
     }
 }
